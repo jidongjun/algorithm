421f7ccb4c459203459c8218a8b9cccfe248c520
jiddong149 <jiddong149@naver.com>
2025-05-22 00:35:35 +0900

    [취업과 이직을 위한 프로그래머스 코딩테스트 문제 풀이 전략] 문자열 내 p와 y의 개수 ( 책 풀이 확인 필요)

--- src/Programmers/chapter4/section4_2_3/Problem11.java
+++ src/Programmers/chapter4/section4_2_3/Problem11.java
@@ -1,4 +1,30 @@
 package Programmers.chapter4.section4_2_3;
 
 public class Problem11 {
+
+    public boolean solution(String s){
+        char[] charArray = s.toCharArray();
+        int pCount = 0;
+        int yCount = 0;
+        for(char c: charArray){
+            if(c == 'p' || c == 'P'){
+                pCount++;
+            } else if(c == 'y' || c == 'Y'){
+                yCount++;
+            }
+        }
+
+        if(pCount == yCount){
+            return true;
+        } else if(pCount != yCount){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Problem11 p = new Problem11();
+        System.out.println(p.solution("qwerrqweqwe"));
+    }
 }
