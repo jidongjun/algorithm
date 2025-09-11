acd1218ea5adac769f5ea94e241b6ade1f2e5060
jiddong149 <jiddong149@naver.com>
2025-09-12 00:23:24 +0900

    [백준] dfs, 벡트레킹, 순열 15650

---
+++ src/bakjoon/Problem_15650.java
@@ -0,0 +1,40 @@
+package bakjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class Problem_15650 {
+    static int N, M;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        arr = new int[M];
+
+        dfs(1, 0);
+        System.out.println(sb.toString());
+    }
+
+    public static void dfs(int start, int depth){
+        if(depth == M){
+            for(int num : arr){
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i <= N; i++){
+            arr[depth] = i;
+            dfs(i + 1, depth + 1);
+        }
+    }
+}
