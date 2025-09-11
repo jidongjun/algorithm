6bf4c987c8d9037b24579c47d5fa002a9226d531
jiddong149 <jiddong149@naver.com>
2025-09-12 00:23:28 +0900

    [백준] dfs, 벡트레킹, 순열 15650

--- src/bakjoon/Problem_15649.java
+++ src/bakjoon/Problem_15649.java
@@ -12,6 +12,7 @@ public class Problem_15649 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
@@ -19,27 +20,28 @@ public class Problem_15649 {
         arr = new int[M];
 
         dfs(0);
+
+        System.out.println(sb);
     }
 
 
     public static void dfs (int depth){
-        if(depth == N){
-            for ( int val : arr){
-                sb.append(val).append(" ");
+            if(depth == M){
+                for ( int val : arr){
+                    sb.append(val).append(" ");
+                }
+                sb.append("\n");
+                return;
             }
-            sb.append("\n");
-            return;
-        }
-
 
             // 1부터 N까지 숫자를 돌면서
             for (int i = 1; i <= N; i++) {
-                if (!visited[i]) {        // 아직 사용하지 않은 숫자라면
-                    visited[i] = true;    // 사용 체크
-                    arr[depth] = i;       // 현재 위치에 숫자 넣기
+                if (!visited[i]) {              // 아직 사용하지 않은 숫자라면
+                    visited[i] = true;          // 사용 체크
+                    arr[depth] = i;             // 현재 위치에 숫자 넣기
                     dfs(depth + 1);       // 다음 깊이 탐색
-                    visited[i] = false;   // 원상복구 (백트래킹)
-                }
+                    visited[i] = false;         // 원상복구 (백트래킹)
             }
         }
     }
+}
