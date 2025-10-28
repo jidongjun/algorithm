f8dd97f3439d4a1c173bb74ca77c1e647d3fd335
Concentration <37768981+jidongjun@users.noreply.github.com>
2025-10-29 01:32:35 +0900

    [백준] DP, 1149번 문제

---
+++ src/bakjoon/DP/Problem_1149.java
@@ -0,0 +1,39 @@
+package bakjoon.DP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_1149 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] cost = new int[N][3];   // 색칠 비용
+        int[][] dp = new int[N][3];     // 최소비용 DP 테이블
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            cost[i][0] = Integer.parseInt(st.nextToken());
+            cost[i][1] = Integer.parseInt(st.nextToken());
+            cost[i][2] = Integer.parseInt(st.nextToken());
+        }
+
+        // 첫번째 집은 각자 비용 그대로
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = cost[i][0] + Math.min(dp[i - 1][1], dp[i - 1][2]);   // 빨강
+            dp[i][1] = cost[i][1] + Math.min(dp[i - 1][0], dp[i - 1][2]);   // 초록
+            dp[i][2] = cost[i][2] + Math.min(dp[i - 1][0], dp[i - 1][1]);   // 파랑
+        }
+
+
+        int result = Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]);
+        System.out.println(result);
+    }
+}
