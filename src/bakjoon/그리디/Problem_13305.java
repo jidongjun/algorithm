6c5c55bb3f1eeaade7892d92f9e2a1bb166f8600
Concentration <37768981+jidongjun@users.noreply.github.com>
2025-12-16 01:52:54 +0900

    [백준] 그리디, 13305번 문제

---
+++ src/bakjoon/그리디/Problem_13305.java
@@ -0,0 +1,35 @@
+package bakjoon.그리디;
+
+import java.util.*;
+import java.io.*;
+
+public class Problem_13305 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] dist = new int[n-1];
+
+        for(int i = 0; i < n; i++){
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int[] price = new int[n];
+        for(int j = 0; j <= n; j++){
+            price[j]= Integer.parseInt(st.nextToken());
+        }
+
+        long minPrice = price[0];
+        long totalCost = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (price[i] < minPrice) {
+                minPrice = price[i];
+            }
+
+            totalCost += minPrice * dist[i];
+        }
+    }
+}
