e422037a1d6f2ab2618417467a21582372a2390f
Concentration <37768981+jidongjun@users.noreply.github.com>
2026-01-21 00:45:01 +0900

    [백준] 정렬, 2750번

---
+++ src/bakjoon/정렬/Problem_2750.java
@@ -0,0 +1,25 @@
+package bakjoon.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Problem_2750 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        while (N-- > 0) {
+            arr.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(arr);
+        arr.forEach(System.out::println);
+    }
+}
